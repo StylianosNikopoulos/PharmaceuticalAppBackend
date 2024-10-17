58c84e3d0c05cfda07056bafc59f5aede346d761
Iraklis Angeloudis <62808163+iraklisangeloudis@users.noreply.github.com>
2024-10-17 21:05:07 +0300

    Implement index function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index(){
-
+        $getProducts = Product::all();
+       
+        if($getProducts->isEmpty()){
+            return response()->json([
+                'message' => 'No products found',
+                'products' => []
+            ],404);
+        }
+        
+        return response()->json([
+            'message' => 'Products successfully retrieved',
+            'products' => $getProducts
+        ],200);
     }
 
     public function show(){
