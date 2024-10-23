7341072c6c8d7c418990848a05668b3341a51329
Shockrates <blackhound55@gmail.com>
2024-10-23 21:44:10 +0300

    Added Post and Put Test

--- tests/Unit/ProductControllerTest.php
+++ tests/Unit/ProductControllerTest.php
@@ -56,4 +56,90 @@ class ProductControllerTest extends TestCase
         $response = $this->getJson('/api/products/{$product->id}');
         $response->assertStatus(404);
     }
+
+    public function test_should_return_created_product()
+    {
+        // Prepare the data for the product.
+        $data = [
+            "name"=> "Sample Product",
+            "category"=> "Capsule",
+            "active_ingredients"=>"Silicon Dioxide",
+            "batch_number"=> "731635212",
+            "manufacturing_date"=> "2024-10-01",
+            "expiration_date"=> "2025-10-01",  
+            "status"=> "under development" 
+        ];
+
+        // Send a POST request to the product creation route.
+        $response = $this->postJson('/api/products', $data);
+        // Check if the response status is 201 (Created).
+        $response->assertStatus(201);
+        // Check if the response contains the expected JSON structure.
+        $response->assertJson([
+            'message' => 'Product created successfully',
+            'product' => [
+                "name"=> "Sample Product",
+                "category"=> "Capsule",
+                "active_ingredients"=>"Silicon Dioxide",
+            ]
+        ]);
+        // Check that the product was created in the database.
+        $this->assertDatabaseHas('products',  [
+            "name"=> "Sample Product",
+            "active_ingredients"=>"Silicon Dioxide",
+        ]);
+    }
+
+    public function test_should_return_updated_product()
+    {
+        //Create a product in the database.
+        $product = Product::create([
+            "name"=> "Sample Product",
+            "category"=> "Capsule",
+            "active_ingredients"=>"Silicon Dioxide",
+            "batch_number"=> "731635212",
+            "manufacturing_date"=> "2024-10-01",
+            "expiration_date"=> "2025-10-01",  
+            "status"=> "under development" 
+        ]);
+
+        // Data to update the product.
+        $updateData = [
+            "name"=> "Updated Product", //Updated Field
+            "category"=> "Capsule",
+            "active_ingredients"=>"Silicon Dioxide",
+            "batch_number"=> "731635212",
+            "manufacturing_date"=> "2024-10-01",
+            "expiration_date"=> "2025-10-01",  
+            "status"=> "completed" //Updated Field
+        ];
+
+        // Act: Send a PUT request to update the product.
+        $response = $this->putJson("/api/products/{$product->id}", $updateData);
+
+        // Assert: Check if the response status is 200 (OK).
+        $response->assertStatus(200);
+
+        // Assert: Check if the response contains the updated product data.
+        $response->assertJson([
+            'message' => 'Product updated successfully',
+            'product' => [
+                "name"=> "Updated Product",
+                "category"=> "Capsule",
+                "active_ingredients"=>"Silicon Dioxide",
+                "batch_number"=> "731635212",
+                "manufacturing_date"=> "2024-10-01",
+                "expiration_date"=> "2025-10-01",  
+                "status"=> "completed"
+            ]
+        ]);
+
+        // Assert: Check if the product was updated in the database.
+        $this->assertDatabaseHas('products', [
+            'id' => $product->id,
+            "name"=> "Updated Product",
+            "status"=> "completed" 
+        ]);
+    }
 }
+
